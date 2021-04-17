70d74d5639e4c901f9af7234154bb7f738da376d
Gi <piresgiovanna02@hotmail.com>
2021-04-17 11:57:17 -0300

    hospitaisrespiradores

---
+++ hospitaisrespiradores.py
@@ -0,0 +1,47 @@
+qrH1 = int(input("Informe a quantidade de respiradores do hospital 1: "))
+toH1 = int(input("Informe a taxa ocupacional do hospital 1: "))
+qrH2 = int(input("Informe a quantidade de respiradores do hospital 2: "))
+toH2 = int(input("Informe a taxa ocupacional do hospital 2: "))
+qrH3 = int(input("Informe a quantidade de respiradores do hospital 3: "))
+toH3 = int(input("Informe a taxa ocupacional do hospital 3: "))
+qrH4 = int(input("Informe a quantidade de respiradores do hospital 4: "))
+toH4 = int(input("Informe a taxa ocupacional do hospital 4: "))
+qrH5 = int(input("Informe a quantidade de respiradores do hospital 5: "))
+toH5 = int(input("Informe a taxa ocupacional do hospital 5: "))
+#Hospital 1
+if qrH1 < 50 and toH1 > 60:
+    qrH1 += 5
+    print("O hospital 1 recebeu mais 5 respiradores, portanto, agora tem ", qrH1, " respiradores")
+else:
+    print("O hospital não precisou de mais respiradores, total: ", qrH1)
+#Hospital 2
+if qrH2 < 50 and toH2 > 60:
+    qrH2 += 5
+    print("O hospital 2 recebeu mais 5 respiradores, portanto, agora tem ", qrh2, " respiradores")
+else:
+    print("O hospital não precisou de mais respiradores, total: ", qrH2)
+#Hospital 3
+if qrH3 < 50 and toH3 > 60:
+    qrH3 += 5
+    print("O hospital 3 recebeu mais 5 respiradores, portanto, agora tem ", qrH3, " respiradores")
+else:
+    print("O hospital não precisou de mais respiradores, total: ", qrH3)
+#Hospital 4
+if qrH4 < 50 and toH4 > 60:
+    qrH4 += 5
+    print("O hospital 4 recebeu mais 5 respiradores, portanto, agora tem ", qrH4, " respiradores")
+else:
+    print("O hospital não precisou de mais respiradores, total: ", qrH4)
+#Hospital 5
+if qrH5 < 50 and toH5 > 60:
+    qrH5 += 5
+    print("O hospital 5 recebeu mais 5 respiradores, portanto, agora tem ", qrH5, " respiradores")
+else:
+    print("O hospital não precisou de mais respiradores, total: ", qrH5)
+
+
+
+
+
+
+
